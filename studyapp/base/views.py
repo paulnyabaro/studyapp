03abcc396cc031f19d5ae86e99fa801ff739c525
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-05 10:03:19 +0300

    Added navbar

--- studyapp/base/views.py
+++ studyapp/base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Home")
+    return render(request, 'base.html')
 
 
 def room(request):
