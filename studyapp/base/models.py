f65d1ae4938fde287d703c277f229327e63558ce
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-06 07:22:12 +0300

    Created Room model

--- studyapp/base/models.py
+++ studyapp/base/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    # host = 
+    # topic =
+    name = models.CharField(max_length=200)
+    description = models.CharField(null=True, blank=True)  
--- studyapp/base/views.py
+++ studyapp/base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'base/home.html')
 
 
 def room(request):
