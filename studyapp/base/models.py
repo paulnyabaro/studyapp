781abf38f3c162e73d1b8e32a7a0160b3e07dbbd
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-06 08:10:53 +0300

    Updated room attributes

--- studyapp/base/models.py
+++ studyapp/base/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Room(models.Model):
-    # host = 
-    # topic =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True) # Does not delete topic
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)  
     # participants = 
@@ -11,3 +18,14 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE) # One to many relationship
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True) # Updates value only once
+    updated = models.DateTimeField(auto_now=True) # Updated every time it is saved
+
+    def __str__(self):
+        return self.body[0:50] # First 50 characters
