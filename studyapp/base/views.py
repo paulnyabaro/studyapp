48bc4b6c5bc98e96e5bb5d8283963fa29ebd9d2f
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-07 06:49:11 +0300

    Updated edit room

--- studyapp/base/views.py
+++ studyapp/base/views.py
@@ -29,5 +29,11 @@ def create_room(request):
 def update_room(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+            
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
