fe98cc1e00c069ee2825954f2796393c1a961f78
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-08 18:14:09 +0300

    Added name and description search

--- studyapp/base/views.py
+++ studyapp/base/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q) # __ means going back to parent, icontains -> the i is used for case insensitivity
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | 
+        Q(name__icontains=q) | 
+        Q(description__icontains=q))
+         # __ means going back to parent, icontains -> the i is used for case insensitivity
     topics = Topic.objects.all()
     context =  {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
