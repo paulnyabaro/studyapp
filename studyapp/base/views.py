0e6e046b5772959c295987a465c3fc968600a7ee
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-09 08:50:51 +0300

    Restricting edit and deletion of rooms to only authenticated users

--- studyapp/base/views.py
+++ studyapp/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
@@ -68,6 +69,10 @@ def create_room(request):
 def update_room(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('You do not have the rights to do that')
+
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
         if form.is_valid():
