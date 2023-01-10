17147d8a51d9e092564bb50d7f1b6afef2d0036c
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-10 08:04:06 +0300

    Added static files

--- studyapp/base/views.py
+++ studyapp/base/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 
@@ -73,6 +73,16 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('created') # Querying child models. Models name in lowercase then underscore set (Set of messages related to this room)
+    
+    
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+    
     context = {'room': room, 'room_messages': room_messages}
     return render(request, 'base/rooms.html', context)
 
