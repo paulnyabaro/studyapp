cc4ec159c6e331eb124aaec3190a71339642f501
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-11 06:44:15 +0300

    Filtering reent activities based on topic

--- studyapp/base/views.py
+++ studyapp/base/views.py
@@ -65,7 +65,7 @@ def home(request):
          # __ means going back to parent, icontains -> the i is used for case insensitivity
     topics = Topic.objects.all()
     room_count = rooms.count()
-    room_messages = Message.objects.filter(Q(room__name__icontains=q)) # Filtering recent activities bases on topic
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q)) # Filtering recent activities based on topic
 
     context =  {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
