0feb609c87f8ef22a4937e9af0f27f82610ea1dc
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-12 10:20:45 +0300

    Created a data list for adding room topics

--- studyapp/base/views.py
+++ studyapp/base/views.py
@@ -99,6 +99,7 @@ def user_profile(request, pk):
 @login_required(login_url='login') # Redirects non logged in users
 def create_room(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
@@ -107,7 +108,7 @@ def create_room(request):
             room.save()
             return redirect('home')
 
-    context = {'form': form}
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login')
