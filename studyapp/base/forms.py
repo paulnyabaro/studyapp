3aa0d37d0b71294360324342883ead760d589b7a
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-13 08:29:08 +0300

    Added user form

--- studyapp/base/forms.py
+++ studyapp/base/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__' # Will create form based on the model content (Rendering all)
         exclude = ['host', 'participants']
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = ['username', 'email'] # The fields you want to be output
