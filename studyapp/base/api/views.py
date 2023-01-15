388a46416a1e8ecbc0f5c9cf2274ec86fca8f349
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-15 06:28:38 +0300

    Creating apis

---
+++ studyapp/base/api/views.py
@@ -0,0 +1,9 @@
+from django.http import JsonResponse
+
+
+def get_routes(request): # A view that shows us all the routes in our API
+    routes = [
+        'GET /api/rooms',
+        'GET /api/rooms/:id'
+    ]
+    return JsonResponse(routes, safe=False)
