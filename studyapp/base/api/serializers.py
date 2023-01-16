bdf1d7d4572d307205249b003d2129a09ed86a86
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-16 05:48:00 +0300

    Added room serializer

--- studyapp/base/api/serializers.py
+++ studyapp/base/api/serializers.py
@@ -1,1 +1,10 @@
-# Takes in an object from response in vies and serializes it
+# Takes in an object from the model and serializes it to be used in the response view
+
+from rest_framework.serializers import ModelSerializer
+from base.models import Room
+
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
--- studyapp/base/api/views.py
+++ studyapp/base/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from base.models import Room
+from .serializers import RoomSerializer
 
 
 @api_view(['GET']) # Only requests allowed e.g 'GET', 'PUT', 'POST'
@@ -15,4 +16,5 @@ def get_routes(request): # A view that shows us all the routes in our API
 @api_view(['GET'])
 def get_rooms(request):
     rooms = Room.objects.all()
-    return Response(rooms)
+    serializer = RoomSerializer(rooms, many=True) 
+    return Response(serializer.data)
