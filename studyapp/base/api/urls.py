7cec5565ffe24221128bb0f49d61542c2dd500fd
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-16 05:52:36 +0300

    Added api url for single room

--- studyapp/base/api/urls.py
+++ studyapp/base/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.get_routes),
     path('rooms/', views.get_rooms),
+    path('rooms/<str:pk>/', views.get_room),
 ]
--- studyapp/base/api/views.py
+++ studyapp/base/api/views.py
@@ -18,3 +18,9 @@ def get_rooms(request):
     rooms = Room.objects.all()
     serializer = RoomSerializer(rooms, many=True) 
     return Response(serializer.data)
+
+@api_view(['GET'])
+def get_room(request, pk):
+    room = Room.objects.get(id=pk)
+    serializer = RoomSerializer(room, many=False) 
+    return Response(serializer.data)
