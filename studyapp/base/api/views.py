640b4f1a1e15423d1b2366e9941894fd0bfb600b
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-16 05:30:03 +0300

    Registered rest_framework

--- studyapp/base/api/views.py
+++ studyapp/base/api/views.py
@@ -1,10 +1,12 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(['GET']) # Only requests allowed e.g 'GET', 'PUT', 'POST'
 def get_routes(request): # A view that shows us all the routes in our API
     routes = [
         'GET /api',
         'GET /api/rooms',
         'GET /api/rooms/:id'
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
