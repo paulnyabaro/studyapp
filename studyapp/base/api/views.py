d210dbbd2d5df66680fb9953160e888f299844cc
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-16 05:16:02 +0300

    Adding api urls

--- studyapp/base/api/views.py
+++ studyapp/base/api/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 
 def get_routes(request): # A view that shows us all the routes in our API
     routes = [
+        'GET /api',
         'GET /api/rooms',
         'GET /api/rooms/:id'
     ]
