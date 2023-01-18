14002801cf7a7ae3d328775f85e33862561c8a34
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-18 07:29:26 +0300

    Moved static files to the correct folder

--- studyapp/studyapp/settings.py
+++ studyapp/studyapp/settings.py
@@ -127,10 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [  # Registering static files
     BASE_DIR / 'static'
 ]
+
+# User uploaded content  
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # STATIC_ROOT =   # For user uploaded images
 
--- studyapp/studyapp/urls.py
+++ studyapp/studyapp/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings # Configuring static urls
+from django.conf.urls.static import static # Importing static method
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
     path('api/', include('base.api.urls')),
 ]
+
+# Setting up static urls
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
