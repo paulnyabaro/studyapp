0aa9edc540cd0a087b78c8254006983e1698ae6c
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-18 19:27:42 +0300

    Adding custom user creation form

--- studyapp/base/views.py
+++ studyapp/base/views.py
@@ -4,9 +4,8 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message, User
-from .forms import RoomForm, UserForm
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 
 def login_page(request):
@@ -39,10 +38,10 @@ def logout_page(request):
     return redirect('home')
 
 def register_page(request):
-    form = UserCreationForm()
+    form = MyUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
